527c974481dde08dfdc89d9612f3e55c365df312
anitptit <an.truong@techupcorp.com>
2022-05-06 15:38:57 +0700

    Setup files NerdCard and and create common component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from './features/NerdCard/pages/Layout'
 
 const App = () => {
   return (
-    <h1 className="h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to-purple-700 text-white">
-      Hello friend! My  name is an.
-    </h1>
+    <div>
+      <Layout />
+    </div>
   )
 }
 
---
+++ src/features/NerdCard/useDarkMode.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect} from 'react'
+
+const useDarkMode = () => {
+  return (
+    <div>useDarkMode</div>
+  )
+}
+
+export default useDarkMode
