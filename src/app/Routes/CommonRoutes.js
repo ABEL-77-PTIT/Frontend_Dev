533e042cbec440fc9076135dcc2109a0beb28d56
AbelTruong <abeltruong.arena@gmail.com>
2022-09-24 14:03:51 +0700

    update source

--- src/app/Routes/CommonRoutes.js
+++ src/app/Routes/CommonRoutes.js
@@ -8,8 +8,6 @@ import BlogsManagement from "../../features/MyBlogs/BlogsManagement";
 import NotFoundPage from "../../features/Error/components/NotFound";
 import CitiAltoManagement from "../../features/CitiAlto/CitiAltoManagement";
 import MainPage from "../../features/PetEcommerce/client/pages/MainPage";
-import MovieAppManagement from "../../features/MovieApp/MovieAppManagement";
-import App from "../../features/TodoApp/App";
 import SkillineManagement from "../../features/Skilline/SkillineManagement";
 
 const CommonRoutes = () => {
@@ -30,12 +28,13 @@ const CommonRoutes = () => {
                     <Route path="nerd_card" element={<NerdCard />} />
                     <Route path="coffee_shop" element={<Coffee />} />
                     <Route path="antube" element={<MyChannel />} />
-                    <Route path="real_estate/*" element ={<CitiAltoManagement />} />
+                    <Route
+                        path="real_estate/*"
+                        element={<CitiAltoManagement />}
+                    />
                     <Route path="blogs" element={<BlogsManagement />} />
                 </Route>
                 <Route path="pet_shop" element={<MainPage />} />
-                <Route path="movie_app" element={<MovieAppManagement/>} />
-                <Route path="todo_app" element={<App />} />
                 <Route path="skilline" element={<SkillineManagement />} />
             </Route>
             <Route path="*" element={<NotFoundPage />} />
--- src/features/Common/Layout/Home.jsx
+++ src/features/Common/Layout/Home.jsx
@@ -171,66 +171,10 @@ const Home = () => {
                                 </h5>
                             </Link>
                             <p className="mb-10 font-normal text-gray-700 dark:text-gray-400">
-                                MERN stack. Ecommerce website basic.
+                                PERN stack. Ecommerce website basic sale guitar.
                             </p>
                             <Link
                                 to="pet_shop"
-                                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            >
-                                Views UI details.
-                                <svg
-                                    className="ml-2 -mr-1 w-4 h-4"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"
-                                    ></path>
-                                </svg>
-                            </Link>
-                        </div>
-                        <div className="p-6 max-w-full lg:mb-0 bg-white rounded-lg border border-gray-200 shadow-md ">
-                            <Link to="movie_app">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Movie App
-                                </h5>
-                            </Link>
-                            <p className="mb-10 font-normal text-gray-700 dark:text-gray-400">
-                                Free movie channel, update new movies everyday
-                            </p>
-                            <Link
-                                to="movie_app"
-                                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            >
-                                Views UI details.
-                                <svg
-                                    className="ml-2 -mr-1 w-4 h-4"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"
-                                    ></path>
-                                </svg>
-                            </Link>
-                        </div>
-                        <div className="p-6 max-w-full lg:mb-0 bg-white rounded-lg border border-gray-200 shadow-md ">
-                            <Link to="todo_app">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Todo App
-                                </h5>
-                            </Link>
-                            <p className="mb-10 font-normal text-gray-700 dark:text-gray-400">
-                                Used reactjs, react-redux, redux-tookit, antd.
-                            </p>
-                            <Link
-                                to="todo_app"
                                 className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             >
                                 Views UI details.
