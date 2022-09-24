4b6537bc8f099da5cfe088391dbcc6a4050c6a64
AbelTruong <abeltruong.arena@gmail.com>
2022-09-24 14:41:38 +0700

    test git lan 3

--- src/features/PetEcommerce/client/pages/test.js
+++ src/features/PetEcommerce/client/pages/test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const test = () => {
-    return <div>co thay doi</div>;
+    return <div>co thay doi. ban than oi duyen so sinh ra chungs minh</div>;
 };
 
 export default test;
